aa16a7b43c8a6ebf1638a6609733c82e3b7ca968
Ajay THombare <ajaypt1942@gmail.com>
2024-03-27 11:50:51 +0530

    Updated Responsive Directive to work on Mobile devices

--- src/app/directive/responsive.directive.ts
+++ src/app/directive/responsive.directive.ts
@@ -1,27 +1,30 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[pageResponsive]'
 })
-export class ResponsiveDirective {
+export class ResponsiveDirective implements OnInit{
   constructor(
     private element: ElementRef,
     private breakpointObserver: BreakpointObserver
   ) {
+  }
+  ngOnInit(): void {
     this.breakpointObserver
-      .observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape])
-      .subscribe({
-        next: (result: any) => {
-          for (let breakpoint of Object.keys(result.breakpoints))
-            if (result.breakpoints[breakpoint]) {
-              if (breakpoint === Breakpoints.HandsetPortrait)
-                this.element.nativeElement.classList.remove('pc');
+    .observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape])
+    .subscribe({
+      next: (result: any) => {
+        for (let breakpoint of Object.keys(result.breakpoints))
+          if (result.breakpoints[breakpoint]) {
+            console.log(breakpoint);
+            if (breakpoint === Breakpoints.HandsetPortrait)
+              this.element.nativeElement.classList.remove('pc');
 
-              if (breakpoint === Breakpoints.WebLandscape)
-                this.element.nativeElement.classList.add('pc');
-            }
-        },
-      });
+            if (breakpoint === Breakpoints.WebLandscape)
+              this.element.nativeElement.classList.add('pc');
+          }
+      },
+    });
   }
 }
